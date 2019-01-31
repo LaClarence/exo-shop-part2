40dd76a7f1cd980f8a6225fcf5811e99056906fe
Lego <LaClarence@users.noreply.github.com>
2019-01-31 15:13:12 +0100

    Dispatch index routes using Express routes

---
+++ routes/department.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../models/product");
+const Department = require("../models/department");
+const Category = require("../models/category");
+
+router.post("/department/create", async (req, res) => {
+  try {
+    for (let i = 0; i < req.body.length; i++) {
+      const department = await Department.findOne({
+        title: req.body[i].title
+      });
+      if (department === null) {
+        const newDepartment = new Department({
+          title: req.body[i].title
+        });
+        await newDepartment.save();
+      } else {
+        console.log(`Departement ${req.body[i].title} already existing.`);
+      }
+    }
+    res.json({ message: "Departments created" });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+});
+
+router.get("/department", async (req, res) => {
+  try {
+    const departments = await Department.find();
+    res.json(departments);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+});
+
+router.post("/department/update", async (req, res) => {
+  // req.query.id
+
+  try {
+    const department = await Department.findById(req.query.id);
+
+    // Est-ce que department existe ?
+    if (department) {
+      department.title = req.body.title;
+      await department.save();
+      res.json(department);
+    } else {
+      res.status(400).json({
+        message: "Department not found"
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+});
+
+router.post("/department/delete", async (req, res) => {
+  try {
+    const department = await Department.findById(req.query.id);
+    if (department) {
+      await department.remove();
+
+      // Supprimer aussi les categories
+      const categories = await Category.find({
+        department: req.query.id
+      });
+
+      // Version 1
+      // for (let i = 0; i < categories.length; i++) {
+      //   await categories[i].remove();
+      // }
+      // Version 2
+      await categories.remove();
+
+      // TODO
+      // Delete categories
+
+      res.json({ message: "Department removed" });
+    } else {
+      res.status(400).json({
+        message: "Department not found"
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+});
+
+module.exports = router;
